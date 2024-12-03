86777cbe023b04726d2339c3468113d319535998
Lindner_Mario <mario.l.lindner@gmail.com>
2024-12-03 20:21:07 -0300

    Intento de Dockerizar

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,7 +24,7 @@ import { CotizacionIndiceModule } from './Entities/IndiceCotizacion/IndiceCotiza
       username: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DB,
-      synchronize: false,
+      synchronize: true,
       entities: ["dist/**/**.entity{.ts,.js}"],
       logging: 'all',
     }),
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4321',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'FETCH'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
   });
